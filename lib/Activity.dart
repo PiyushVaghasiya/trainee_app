cd9b7b9da6cc52af0900308ac6bfcee5d6cc536c
Piyush Vaghasiya <piyushvaghasiya2906@gmail.com>
2021-11-24 17:49:34 +0530

    Activity page

--- lib/Activity.dart
+++ lib/Activity.dart
@@ -30,25 +30,129 @@ class _ActivityState extends State<Activity>
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
-        title: Text("Activity",
-            style: TextStyle(
-              color: Colors.white,
-            ),textAlign: TextAlign.left,),
+        title: Text(
+          "Activity",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+          textAlign: TextAlign.left,
+        ),
       ),
-      body: Container(color: Colors.black,
+      body: Container(
+        color: Colors.black,
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Row(
               children: [
-               Container(height: 100,width: 100,
-                 decoration: BoxDecoration(
-                   image: DecorationImage(
-                     image: NetworkImage("https://cdn.vectorstock.com/i/thumb-large/94/38/avatar-flat-icon-on-black-background-black-style-vector-25959438.jpg")
-                   ),
-                 ),
-               ),
+                Container(
+                  height: 70,
+                  width: 70,
+                  margin: EdgeInsets.only(left: 5),
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              "https://cdn.vectorstock.com/i/thumb-large/94/38/avatar-flat-icon-on-black-background-black-style-vector-25959438.jpg")),
+                      shape: BoxShape.circle,
+                      border: Border.all(color: Colors.white, width: 1)),
+                ),
+                Expanded(
+                  child: Container(
+                    alignment: Alignment.centerLeft,
+                    margin: EdgeInsets.all(10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Follow requests",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(
+                          "Approve or ignore requests",
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ],
-            )
+            ),
+            Container(
+              margin: EdgeInsets.only(
+                left: 10,
+                top: 10,
+                bottom: 10,
+              ),
+              child: Text(
+                "Today",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 17,
+                ),
+                textAlign: TextAlign.left,
+              ),
+            ),
+            Row(
+              children: [
+                Container(
+                  height: 70,
+                  width: 70,
+                  margin: EdgeInsets.only(left: 5),
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              "https://images.pexels.com/photos/1704488/pexels-photo-1704488.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500")),
+                      shape: BoxShape.circle,
+                      border: Border.all(color: Colors.white, width: 1)),
+                ),
+                Expanded(
+                  child: Container(
+                    alignment: Alignment.centerLeft,
+                    margin: EdgeInsets.all(10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        RichText(
+                          text: TextSpan(
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.white,
+                              ),
+                              children: [
+                                TextSpan(
+                                  text: "||__mr.nik__||",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                TextSpan(
+                                  text:
+                                      " commented on a post that you're tagged in",
+                                ),
+                              ]),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 60,
+                  width: 60,
+                  margin: EdgeInsets.only(left: 5),
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: NetworkImage(
+                            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR10hkHFV6UitNs7ArQUbAmJGZtn-NIzXPPLRgUq7K-4zt6DcgeSaUzgCaLh49iMx4MN4c&usqp=CAU")),
+                    ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
