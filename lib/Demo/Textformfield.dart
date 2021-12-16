47170fdf138f2aaa02c732616074d1706c835909
Piyush Vaghasiya <piyushvaghasiya2906@gmail.com>
2021-12-16 15:15:11 +0530

    textformfield and foemfield

--- lib/Demo/Textformfield.dart
+++ lib/Demo/Textformfield.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-class Textformfield extends StatefulWidget {
-  const Textformfield({Key? key}) : super(key: key);
+class Textformfield_demo extends StatefulWidget {
+  const Textformfield_demo({Key? key}) : super(key: key);
 
   @override
-  _TextformfieldState createState() => _TextformfieldState();
+  _Textformfield_demoState createState() => _Textformfield_demoState();
 }
 
-class _TextformfieldState extends State<Textformfield>
+class _Textformfield_demoState extends State<Textformfield_demo>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
 
@@ -23,26 +23,48 @@ class _TextformfieldState extends State<Textformfield>
     super.dispose();
   }
 
-  final key = GlobalKey<FormState>();
-  TextEditingController emailcontroller = TextEditingController();
+  final formkey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-      body: Container(
-        child: Column(
-          children: [
-            Form(
-              key: key,
+      child: Scaffold(
+        body: Center(
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Form(
+              key: formkey,
               child: Column(
-                children: [Textformfield(
-                )],
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(color: Colors.red)),
+                        hintText: "search",
+                        labelText: "ssearch",
+                        hintStyle:
+                            TextStyle(color: Colors.green, fontSize: 15)),
+                  ),
+                  Padding(padding: EdgeInsets.all(20),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          helperText: "name plzzz",
+                          border: OutlineInputBorder(),
+                          labelText: "user name"),
+                      style: TextStyle(
+                        color: Colors.green,
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
+          ),
         ),
       ),
-    ));
+    );
   }
 }
