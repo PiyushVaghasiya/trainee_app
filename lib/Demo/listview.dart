a99c07718aff0f3615d8e3238b4a59786c185498
Piyush Vaghasiya <piyushvaghasiya2906@gmail.com>
2022-01-13 15:31:01 +0530

    addding student class in model class

--- lib/Demo/listview.dart
+++ lib/Demo/listview.dart
@@ -29,13 +29,13 @@ class _ListviewDemoState extends State<ListviewDemo>
     // List<String> name = ["piyush", "dishant", "abc"];
 
     List<Employee> employeelist = [
-      Employee(name: "piyush", married: true, salary: 1000),
-      Employee(name: "dishant", married: true, salary: 1000),
-      Employee(name: "abc", married: true, salary: 1000),
-      Employee(name: "def", married: true, salary: 12200),
-      Employee(name: "ghi", married: false, salary: 1000),
-      Employee(name: "jkl", married: true, salary: 1400),
-      Employee(name: "mno", married: false, salary: 9000),
+      Employee(name: "piyush", married: true, salary: 1000, age: 20),
+      Employee(name: "dishant", married: true, salary: 1000, age: 20),
+      Employee(name: "abc", married: true, salary: 1000, age: 10),
+      Employee(name: "def", married: true, salary: 12200, age: 20),
+      Employee(name: "ghi", married: false, salary: 1000, age: 20),
+      Employee(name: "jkl", married: true, salary: 1400, age: 30),
+      Employee(name: "mno", married: false, salary: 9000, age: 17),
     ];
 
     return Scaffold(
@@ -58,10 +58,15 @@ class _ListviewDemoState extends State<ListviewDemo>
                             EdgeInsets.only(bottom: 10, left: 10, right: 10),
                         color: Colors.blue.shade50,
                         child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             Text(employeelist[index].name ?? ""),
                             Text(employeelist[index].salary.toString()),
                             Text("married :${employeelist[index].married}"),
+                            Text(employeelist[index].age.toString()),
+                            Text(studdata[index].name??""),
+                            Text(studdata[index].rollno.toString()),
+                            Text(studdata[index].marks.toString()),
                           ],
                         )),
                   ),
--- lib/models/employee.dart
+++ lib/models/employee.dart
@@ -2,6 +2,21 @@ class Employee {
   String? name;
   int? salary;
   bool? married;
+  int? age;
 
-  Employee({this.name, this.salary, this.married});
+  Employee({this.name, this.salary, this.married, this.age});
 }
+
+class Student {
+  String? name;
+  int? rollno;
+  int? marks;
+
+  Student({this.name, this.rollno, this.marks});
+}
+
+List<Student> studdata = [
+  Student(name: "abc", marks: 90, rollno: 10),
+  Student(name: "def", marks: 70, rollno: 11),
+  Student(name: "xyz", marks: 80, rollno: 12),
+];
