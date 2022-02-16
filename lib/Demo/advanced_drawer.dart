9e4c95fa80d472152e63bd3439df8cd86560cfb6
Piyush Vaghasiya <piyushvaghasiya2906@gmail.com>
2022-02-16 15:37:49 +0530

    making drawer in advance drawer

--- lib/Demo/advanced_drawer.dart
+++ lib/Demo/advanced_drawer.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_advanced_drawer/flutter_advanced_drawer.dart';
 
@@ -13,39 +14,149 @@ class _AdvancedDrawerDemoState extends State<AdvancedDrawerDemo> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.brown,
-        // backgroundColor:Color(0xFF4E1A0D),
-
-        title: Text(
-          "Home",
-          style: TextStyle(fontFamily: "Rye"),
-        ),
-      ),
-      drawer: AdvancedDrawer(
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text('Advanced Drawer Example'),
-            leading: IconButton(
-              onPressed: _handleMenuButtonPressed,
-              icon: ValueListenableBuilder<AdvancedDrawerValue>(
-                valueListenable: _advancedDrawerController,
-                builder: (_, value, __) {
-                  return AnimatedSwitcher(
-                    duration: Duration(milliseconds: 250),
-                    child: Icon(
-                      value.visible ? Icons.clear : Icons.menu,
-                      key: ValueKey<bool>(value.visible),
-                    ),
-                  );
-                },
-              ),
+    return AdvancedDrawer(
+      backdropColor: Color(0xFAF6F4F4),
+      controller: _advancedDrawerController,
+      animationCurve: Curves.easeInOut,
+      animationDuration: const Duration(milliseconds: 300),
+      animateChildDecoration: true,
+      rtlOpening: false,
+      disabledGestures: false,
+      // childDecoration: const BoxDecoration(
+      //   // NOTICE: Uncomment if you want to add shadow behind the page.
+      //   // Keep in mind that it may cause animation jerks.
+      //   // boxShadow: <BoxShadow>[
+      //   //   BoxShadow(
+      //   //     color: Colors.black12,
+      //   //     blurRadius: 0.0,
+      //   //   ),
+      //   // ],
+      //   borderRadius: const BorderRadius.all(Radius.circular(16)),
+      // ),
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Text(
+            'Hello',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontFamily: "Rye"),
+          ),
+          leading: IconButton(
+            onPressed: _handleMenuButtonPressed,
+            icon: ValueListenableBuilder<AdvancedDrawerValue>(
+              valueListenable: _advancedDrawerController,
+              builder: (_, value, __) {
+                return AnimatedSwitcher(
+                  duration: Duration(milliseconds: 250),
+                  child: Icon(
+                    value.visible ? Icons.clear : Icons.menu,
+                    key: ValueKey<bool>(value.visible),
+                  ),
+                );
+              },
             ),
           ),
-          body: Container(),
         ),
-        drawer: SafeArea(
-          child: Container(),
+        body: Container(),
+      ),
+      drawer: SafeArea(
+        child: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: EdgeInsets.all(20),
+                alignment: Alignment.centerLeft,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      height: 80,
+                      width: 80,
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black, width: 2),
+                          image: DecorationImage(
+                              image: NetworkImage(
+                                  "https://media.wired.com/photos/593261cab8eb31692072f129/master/pass/85120553.jpg"),
+                              fit: BoxFit.cover),
+                          shape: BoxShape.circle),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "Piyush Vaghasiya",
+                      style: TextStyle(
+                          fontFamily: "Rye", color: Colors.black, fontSize: 12),
+                    ),
+                    Text(
+                      "piyushvaghasiya@gmail.com",
+                      style: TextStyle(
+                          fontFamily: "Rye", color: Colors.black, fontSize: 10),
+                    ),
+                  ],
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ListTile(
+                    leading: Icon(
+                      Icons.person,
+                      color: Colors.black,
+                    ),
+                    title: Transform.translate(
+                      offset: Offset(-25, 0),
+                      child: Text(
+                        'Profile',
+                        style: TextStyle(fontFamily: "Rye", fontSize: 12),
+                      ),
+                    ),
+                  ),
+                  ListTile(
+                    leading: Icon(
+                      Icons.home,
+                    ),
+                    title: Transform.translate(
+                      offset: Offset(-25, 0),
+                      child: Text(
+                        'Home',
+                        style: TextStyle(fontFamily: "Rye", fontSize: 12),
+                      ),
+                    ),
+                  ),
+                  ListTile(
+                    leading: Icon(
+                      Icons.shopping_cart,
+                      color: Colors.black,
+                    ),
+                    title: Transform.translate(
+                      offset: Offset(-25, 0),
+                      child: Text(
+                        'Cart',
+                        style: TextStyle(fontFamily: "Rye", fontSize: 12),
+                      ),
+                    ),
+                  ),
+                  ListTile(
+                    leading: Icon(
+                      Icons.logout,
+                      color: Colors.black,
+                    ),
+                    title: Transform.translate(
+                      offset: Offset(-25, 0),
+                      child: Text(
+                        'Logout',
+                        style: TextStyle(fontFamily: "Rye", fontSize: 12),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -57,5 +168,3 @@ class _AdvancedDrawerDemoState extends State<AdvancedDrawerDemo> {
     _advancedDrawerController.showDrawer();
   }
 }
-
-
